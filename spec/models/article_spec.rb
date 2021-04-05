90b4b88a9b316d34f8a8d33bb18ef0e2c63d3196
ch-canaza <ch-canaza@hotmail.com>
2021-04-05 16:49:38 -0400

    test's articles validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'test taht factory is valid' do
-    article = build(:article)
-    expect(article).to be_valid
+  describe '#alidations' do
+    let(:article) { build(:article) }
+
+    it 'test taht factory is valid' do
+      expect(article).to be_valid
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include('can not be blank')
+    end
   end
 end
